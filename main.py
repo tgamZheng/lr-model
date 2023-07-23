627f6406f04571fd13074e25b92d37b197c059bf
ZhengX-94 <xanderxu1994@gmail.com>
2023-07-23 12:49:26 -0400

    add init ver lr model

---
+++ main.py
@@ -0,0 +1,26 @@
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+def train():
+    # Sample data: a simple sequence from 1 to 10
+    X = np.array([[i] for i in range(1, 11)])
+    y = np.array([i * 2 for i in range(1, 11)])
+
+    # Initialize the linear regression model
+    model = LinearRegression()
+
+    # Train the model on the data
+    model.fit(X, y)
+
+    return model
+
+def predict(model,input:int) -> int:
+    # Predict the next value in the sequence (11)
+    next_value = model.predict([[input]])[0]
+    print(f"Predicted next value in the sequence: {next_value:.2f}")
+    return int
+
+
+if __name__ == "__main__":
+    model = train()
+    predict(model, input=11)
