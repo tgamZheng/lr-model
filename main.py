51efc40b3b80f6458d36e022318bb995f5011000
ZhengX-94 <xanderxu1994@gmail.com>
2023-07-23 13:16:55 -0400

    added docstring

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
+"""module doc: this module has lr model train predict func"""
+
 import numpy as np
 from sklearn.linear_model import LinearRegression
 
 def train():
+    """train function
+    Params:
+    a
+    b
+    c
+
+    Returns:
+        _type_: _description_
+    """
     # Sample data: a simple sequence from 1 to 10
     X = np.array([[i] for i in range(1, 13)])
     y = np.array([i * 2 for i in range(1, 13)])
@@ -15,6 +26,15 @@ def train():
     return model
 
 def predict(model,input:int) -> int:
+    """_summary_
+
+    Args:
+        model (_type_): lr model object
+        input (int): an arb int
+
+    Returns:
+        int: predicted value(int)
+    """
     # Predict the next value in the sequence (11)
     next_value = model.predict([[input]])[0]
     print(f"Predicted next value in the sequence: {next_value:.2f}")
